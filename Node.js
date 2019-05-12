fed094c3a68ce8ae0a6027b6aeefb380e090dbb5
ernesahaxhijaj <ernesa.haxhijaj@gmail.com>
2019-05-13 00:49:27 +0200

    detyra5

---
+++ Node.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var mysql = require('mysql');
+
+var app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: 'e-commerce'
+});
+
+con.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected!");
+}); 
+
+
+    app.get('/products', function(req, res) {
+
+        if(req.query.categoryId === ''){
+            con.query("SELECT * FROM produkti", function (err, result, fields) {
+            if (err) throw err;
+            console.log(JSON.stringify(result));
+          });
+        }
+        
+        else{
+        var x= req.query.categoryId;
+        var sql = 'SELECT * FROM produkti WHERE categoryId = ?';
+        con.query(sql, [x], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+});
+
+      app.listen(3000, function () {
+        console.log('Node.js web server at port 3000 is running..')
+    });
